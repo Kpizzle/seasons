fd8cbaf60f912a09c7e8f62212e8986e8749b937
Kyle Potter <kp1289@gmail.com>
2020-09-19 00:29:18 +0100

    created a class and added constructor and state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-useless-constructor */
 import React from "react";
 import ReactDom from "react-dom";
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hello</div>;
+class App extends React.Component {
+  //Not a required app - belongs to JS
+  constructor(props) {
+    //Called before render function, best place to set state
+    //We need to pass props into the constructor
+    //And then calling the parent component with the super function.
+    super(props);
+    this.state = {lat: null};
+  }
+
+  //Must be defined
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
 }
 
-ReactDom.render(<App/ >, document.querySelector("#root"));
+ReactDom.render(<App />, document.querySelector("#root"));
