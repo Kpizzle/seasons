9a6e89da06c980a28d7b9a2d594519ebb0f8dea1
Kyle Potter <kp1289@gmail.com>
2020-09-19 01:01:58 +0100

    added error handle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-constructor */
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 
 class App extends React.Component {
   //Not a required app - belongs to JS
@@ -10,29 +10,36 @@ class App extends React.Component {
     //And then calling the parent component with the super function.
     super(props);
 
-    //ONLY time you can direct assign is on creation. 
-    this.state = {lat: null};
+    //ONLY time you can direct assign is on creation.
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-            //We called set state to update the state of lat
-            //The render function is then updated almost. 
-            this.setState({lat: position.coords.latitude})
+      (position) => {
+        //We called set state to update the state of lat
+        //The render function is then updated almost.
+        this.setState({ lat: position.coords.latitude });
 
-            //NEVER update state object directly -
-            //this.state.lat = position.coords.latitude
-        },
-        (err) => {
-            this.setState({lat: 0})
-        }
-      );
+        //NEVER update state object directly -
+        //this.state.lat = position.coords.latitude
+      },
+      (err) => {
+        console.log(err);
+        this.setState({lat: 0})
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
   //Must be defined
   render() {
-
-    return <div>Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}{' '}
+      </div>
+    );
   }
 }
 
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDom.render(<App />, document.querySelector('#root'));
